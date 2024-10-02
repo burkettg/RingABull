fbca07e8edd45b6a32c6e9d90489d287a116b949
ngbcoder94 <ngbcoder94@gmail.com>
2024-10-01 22:45:35 -0400

    Fixed up the second screen. Now I need to add some game play.

--- RingABull/GameView.swift
+++ RingABull/GameView.swift
@@ -16,26 +16,36 @@ struct GameView: View {
     
     var body: some View{
         
-        //Create a VStack to hold my screens objects... in a way
-        VStack {
-            Text("This is the second screen!")      //Display text on the screen
-                .font(.largeTitle)                  //Define the font for the text
-                .padding()                          //Add some padding
+        ZStack{
             
-            //Define a button
-            Button(action: {
+            //Here I will work with the background/background image
+            Image("GameMode00")
+                .resizable()         //Make the image resizeable to the screen
+                .scaledToFill()      //Nees to make sure the image can fill the screen
+                .ignoresSafeArea()   //Makes the image cover the entire screen!
+            
+            //Create a VStack to hold my screens objects... in a way
+            VStack {
+                Spacer()
+                //Define a button
+                Button(action: {
+                    
+                    //When the user clicks this button, the action will be to dismiss the screen/current view
+                    dismiss()
+                }){
+                    Text("End Game")             //Display text on the screen
+                        .font(.headline)         //Define the font for the text
+                        .padding()               //Add padding around the text
+                        .frame(width: 150, height: 40)      //Add a frame with some dimensions
+                        .background(Color.red)              //Define the background color
+                        .foregroundColor(.white)            //Define the foreground color
+                        .cornerRadius(10)                   //Define the radius of the button
+                        
+                }
+                .padding(.bottom, 10)
                 
-                //When the user clicks this button, the action will be to dismiss the screen/current view
-                dismiss()
-            }){
-                Text("End Game")             //Display text on the screen
-                    .font(.headline)         //Define the font for the text
-                    .padding()               //Add padding around the text
-                    .frame(width: 150, height: 40)      //Add a frame with some dimensions
-                    .background(Color.red)              //Define the background color
-                    .foregroundColor(.white)            //Define the foreground color
-                    .cornerRadius(10)                   //Define the radius of the button
             }
+            
         }
     }
 }
